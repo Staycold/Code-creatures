ffeff5bd1339c0309dedfa1c121303fde3fdfdaa
Elison Liu <elisonliu97@gmail.com>
2021-07-28 12:19:21 -0400

    front end routing and file structure

---
+++ client/src/pages/login/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+
+    return (
+        <main>
+            <div>
+                Login
+            </div>
+        </main>
+    );
+};
+
+export default Login;
---
+++ client/src/pages/profile/profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+
+    return (
+        <main>
+            <div>
+                Profile
+            </div>
+        </main>
+    );
+};
+
+export default Profile;
