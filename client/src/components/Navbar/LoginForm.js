03cd654e8573f57f84fb7a3d9ad3687a43b7f522
Elison Liu <elisonliu97@gmail.com>
2021-07-29 15:30:44 -0400

    basic spriting

---
+++ client/src/components/Navbar/LoginForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LoginForm = () => {
+
+    return (
+        <form>
+            <div>
+                <label> EMAIL </label>
+                <input />
+            </div>
+            <div>
+                <label> PASSWORD </label>
+                <input />
+            </div>
+            <button>SUBMIT</button>
+        </form>
+    );
+}
---
+++ client/src/images/index.js
@@ -0,0 +1,9 @@
+import whitePet1 from './WhitePet1_Png.png'
+import whitePet2 from './WhitePet2_Png.png'
+
+const imgs = {
+    whitePet1,
+    whitePet2,
+}
+
+export default imgs
