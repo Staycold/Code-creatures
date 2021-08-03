7fb8d0a1a2ce7672ef1526d5f76297ff87a1bcd3
Elison Liu <elisonliu97@gmail.com>
2021-08-02 23:05:15 -0400

    styling for storepage

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -5,6 +5,12 @@ import grass from './Grass_Texture.png'
 import cloud1 from './Cloud_01.png'
 import cloud3 from './Cloud_03.png'
 import sun from './sun.png'
+import orange from './orange.png'
+import cherry from './red-cherry.png'
+import watermelon from './watermelon.png'
+import shopkeep1 from './shopkeep1.png'
+import shopkeep2 from './shopkeep2.png'
+import shopkeep3 from './shopkeep3.png'
 
 const imgs = {
     cat,
@@ -13,7 +19,13 @@ const imgs = {
     grass,
     cloud1,
     cloud3,
-    sun
+    sun,
+    orange,
+    cherry,
+    watermelon,
+    shopkeep1,
+    shopkeep2,
+    shopkeep3
 }
 
 export default imgs
