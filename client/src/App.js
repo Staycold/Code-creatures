0883fe77f35111a5f982fa9d25f1b7c582e30099
Emily Gleeson <engleeson@gmail.com>
2021-08-03 13:15:36 -0400

    styling profile page and trying to get keyframes working

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ import Footer from './components/Footer/footer';
 import ChallengeHub from './pages/challengeHub/challengeHub';
 import Store from './pages/store/store'
 import AddChallenge from './pages/Admin/admin';
-//import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 //import './App.css'
 
 // // Construct our main GraphQL API endpoint
